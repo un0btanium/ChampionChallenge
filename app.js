58a8260695f9b2ae1de5a47baae7a72809117a5a
un0btanium <unobtaniumlol@gmail.com>
2016-05-14 00:36:08 +0200

    Fixed error on random site calls

--- app.js
+++ app.js
@@ -58,7 +58,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
+    challenges: {ends: new Date().getTime()},
+    currentDate: new Date().getTime()
   });
 });
 
